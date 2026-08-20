a9ca78a4bc4b0100e39a264944a76e23f322d4cb
jaspreet-anayat <jaspreet@anayatglobalworks.com>
2026-08-20 13:42:26 +0530

    changed images path

---
+++ src/assets/images/index.ts
@@ -0,0 +1,29 @@
+import hospitalityRecruitingSolutions1Img from './hospitality-recruiting-solutions-1.png';
+import ourSpecialtiesImg from './our-specialities.webp';
+import readyToFind1Img from './ready-to-find-1.webp';
+
+export const hospitalityRecruitingSolutions1 = hospitalityRecruitingSolutions1Img;
+export const hospitalityRecruitingSolutions = hospitalityRecruitingSolutions1Img;
+export const heroRecruiting = hospitalityRecruitingSolutions1Img;
+export const ourSpecialties275x300 = ourSpecialtiesImg;
+export const ourSpecialties = ourSpecialtiesImg;
+export const readyToFind1 = readyToFind1Img;
+export const restaurantRecruiting = ourSpecialtiesImg;
+export const hospitalityStaffingSolutions = hospitalityRecruitingSolutions1Img;
+export const hireAChef = ourSpecialtiesImg;
+export const hospitalityExecutiveSearch = hospitalityRecruitingSolutions1Img;
+
+export const IMAGES = {
+  heroRecruitingSolutions1: hospitalityRecruitingSolutions1,
+  heroRecruitingSolutions: hospitalityRecruitingSolutions,
+  heroRecruiting: heroRecruiting,
+  ourSpecialties275x300: ourSpecialties275x300,
+  ourSpecialties: ourSpecialties,
+  readyToFind1: readyToFind1,
+  restaurantRecruiting: restaurantRecruiting,
+  hospitalityStaffingSolutions: hospitalityStaffingSolutions,
+  hireAChef: hireAChef,
+  hospitalityExecutiveSearch: hospitalityExecutiveSearch,
+};
+
+export default IMAGES;
